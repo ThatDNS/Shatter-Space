f66d8ff5bd4baff0da0a7933e6e8f46a7c3470cf
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2023-12-29 18:13:22 -0500

    Create Assets folder

--- Game/Game/Src/Engine/Systems/Engine.cpp
+++ Game/Game/Src/Engine/Systems/Engine.cpp
@@ -1,17 +1,37 @@
 #include "stdafx.h"
 #include "Engine/Systems/Engine.h"
+#include "Engine/Systems/SceneManager.h"
 
 extern void Engine_Register();
 
 void Engine::Initialize()
 {
 	Engine_Register();
+
+	// Load the managers
+	SceneManager::Get().Load();
+
+	// Initialize the managers
+	SceneManager::Get().Initialize();
 }
 
 void Engine::Destroy()
 {
+	SceneManager::Get().Destroy();
 }
 
 void Engine::GameLoop()
 {
+	bool _isRunning = true;
+	while (_isRunning)
+	{
+		// --------------------- Pre-update Phase ---------------------
+		SceneManager::Get().PreUpdate();
+
+		// --------------------- Update Phase ---------------------
+		SceneManager::Get().Update();
+
+		// --------------------- Post-update Phase ---------------------
+		SceneManager::Get().PostUpdate();
+	}
 }
